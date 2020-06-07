2daedc0aec18523935ce14228f617dc0898df8d3
Yegor <ustinov6661990@gmail.com>
2020-06-08 01:07:39 +0800

    Initial commit

---
+++ app/src/main/java/com/lucifer/hackerman/barometer/MainActivity.java
@@ -0,0 +1,48 @@
+package com.lucifer.hackerman.barometer;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor pressure;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float millibarsOfPressure = event.values[0];
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText(String.valueOf(millibarsOfPressure));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
