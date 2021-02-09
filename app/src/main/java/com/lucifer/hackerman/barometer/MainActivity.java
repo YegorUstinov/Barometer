55302b1b46426f22bbe6e9b8e9de36160ca6d528
Yegor <ustinov6661990@gmail.com>
2021-02-10 00:31:58 +0800

    many little changes

--- app/src/main/java/com/lucifer/hackerman/barometer/MainActivity.java
+++ app/src/main/java/com/lucifer/hackerman/barometer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lucifer.hackerman.barometer;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
     }
